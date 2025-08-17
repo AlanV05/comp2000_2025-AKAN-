afa91410ee5871c7790128f86ea9432a883a28ec
Alan V <alan.verghese@students.mq.edu.au>
2025-08-17 19:48:45 +1000

    week 3 stuff

--- src/Main.java
+++ src/Main.java
@@ -10,28 +10,60 @@ public class Main extends JFrame {
       window.run();
     }
 
+    class Cell {
+      int x;
+      int y;
+      int cellHeight;
+      int cellWidth;
+
+      public Cell(int h, int w, int x, int y){
+        this.cellHeight = h;
+        this.cellWidth = w;
+      }
+
+      public void makeCell (Graphics g){
+        g.setColor(java.awt.Color.BLACK);
+        g.drawRect(x, y, cellWidth, cellHeight);
+      }    
+      }
+
+    }
+    class Grid { // to modify the grid specs
+      int posX;
+      int posY;
+      int heigth;
+      int width; 
+      int cellSize;
+
+      public Grid (int posX, int posY, int heigth, int width, int cellSize){
+        this.posX = posX;
+        this.posY = posY; 
+        this.heigth = heigth;
+        this.width = width;
+        this.cellSize = cellSize; 
+      }
+      public void makeGrid (Graphics g){
+        for(int i = posX; i<=width; i+=cellSize){
+          for(int j = posY; j<=heigth; j+=cellSize){
+            g.drawRect(i, j, cellSize, cellSize);
+          }
+        }
+      }
+    
+    }
+
+
     class Canvas extends JPanel {
       public Canvas() {
         setPreferredSize(new Dimension(720, 720));
       }
 
       @Override
-      public void paint(Graphics g) { // paint object to "paint" the grid and cells
-
-  int gridStart = 10;
-  int recSize = 700;
-
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(gridStart, gridStart, recSize, recSize);
-
-  for(int i = 10; i<=recSize; i+=25){ // creating the cells 35x35
-      // vertical lines:
-    g.drawLine(i,gridStart,i,gridStart+recSize);
-    // horizontal lines
-    g.drawLine(gridStart, i, gridStart+recSize, i);
-  } 
-      }
-    }
+      public void paint(Graphics g) {
+          super.paint(g);
+Grid grid = new Grid(10, 10,700, 700, 25);
+        }
+  }
 
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
