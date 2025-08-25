9707be300b56a3ef8090830c9625babb4d2a22dc
Alan V <alan.verghese@students.mq.edu.au>
2025-08-25 12:24:04 +1000

    task 10

--- src/Stage.java
+++ src/Stage.java
@@ -1,23 +1,28 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Stage {
   Grid grid;
-  Actor cat;
-  Actor dog;
-  Actor bird;
+  List<Actor> actors = new ArrayList<>();
 
   public Stage() {
     grid = new Grid();
-    cat = new Cat(grid.cellAtColRow(0, 0));
-    dog = new Dog(grid.cellAtColRow(0, 15));
-    bird = new Bird(grid.cellAtColRow(12, 9));
+
+    // create actors and add them to the list
+    actors.add(new Cat(grid.cellAtColRow(0, 0)));
+    actors.add(new Dog(grid.cellAtColRow(0, 15)));
+    actors.add(new Bird(grid.cellAtColRow(12, 9)));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
+    // paint the grid first
     grid.paint(g, mouseLoc);
-    cat.paint(g);
-    dog.paint(g);
-    bird.paint(g);
+
+    // now loop through all actors and paint them
+    for (Actor actor : actors) {
+      actor.paint(g);
+    }
   }
 }
