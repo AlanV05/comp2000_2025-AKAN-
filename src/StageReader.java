a13d5a338e98931396bf46503f70aeaa453263ea
Alan V <alan.verghese@students.mq.edu.au>
2025-09-08 00:11:37 +1000

    stuff

--- src/StageReader.java
+++ src/StageReader.java
@@ -3,10 +3,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+class StageFormatException extends Exception {
+    public StageFormatException(String message) {
+        super(message);
+    }
 public class StageReader {
   public static Stage readStage(String path) throws IOException {
     Stage stage = new Stage();
     List<String> lines = Files.readAllLines(Paths.get(path));
+    for(String data : lines){
+      if(data.isEmpty()){
+        throw new StageFormatException("no data seen");
+      }
+      
+    }
     return new Stage();
   }
 }
