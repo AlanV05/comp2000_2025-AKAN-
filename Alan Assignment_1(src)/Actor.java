c136d41cfddf69d7470109f048fb9f1e5098c50d
Alan V <alan.verghese@students.mq.edu.au>
2025-09-21 16:18:49 +1000

    Add movement capabilities and item collection to Actor class - implements Movable interface for terrain-aware movement, adds inventory system for collected items, and introduces abstract updateShapePositions() method for dynamic actor positioning

--- Alan Assignment_1(src)/Actor.java
+++ Alan Assignment_1(src)/Actor.java
@@ -2,14 +2,64 @@ import java.awt.Graphics;
 import java.awt.Polygon;
 import java.util.ArrayList;
 
-public abstract class Actor {
-  ArrayList<Polygon> shapes = new ArrayList<>();
-  Cell loc;
+public abstract class Actor implements Movable {
+    ArrayList<Polygon> shapes = new ArrayList<>();
+    Cell loc;
+    protected int movementSpeed = 1;
+    protected ArrayList<Item<?>> inventory = new ArrayList<>();
 
-  public void paint(Graphics g) {
-    for (Polygon P : shapes){
-      g.drawPolygon(P); //outline
-      g.fillPolygon(P); //Filling
+    public void paint(Graphics g) {
+        for (Polygon P : shapes){
+            g.drawPolygon(P); //outline
+            g.fillPolygon(P); //Filling
+        }
     }
-  }
+    
+    @Override
+    public boolean canMoveTo(Cell destination) {
+        if(destination == null) return false;
+        return destination.allowsMovement();
+    }
+    
+    @Override
+    public void moveTo(Cell destination) {
+        if(canMoveTo(destination)) {
+            this.loc = destination;
+            updateShapePositions();
+            checkForItems();
+        }
+    }
+    
+    @Override
+    public int getMovementSpeed() {
+        return movementSpeed;
+    }
+    
+    protected abstract void updateShapePositions();
+    
+    @SuppressWarnings("unchecked")
+    protected void checkForItems() {
+        ArrayList<Item<?>> itemsToRemove = new ArrayList<>();
+        
+        for(Item<?> item : loc.getItems()) {
+            try {
+                CollectibleItem<Actor> collectible = (CollectibleItem<Actor>) item;
+                if(collectible.canBeCollectedBy(this)) {
+                    collectible.onCollected(this);
+                    inventory.add(item);
+                    itemsToRemove.add(item);
+                }
+            } catch(ClassCastException e) {
+                // Item not meant for this actor type
+            }
+        }
+        
+        for(Item<?> item : itemsToRemove) {
+            loc.removeItem(item);
+        }
+    }
+    
+    public ArrayList<Item<?>> getInventory() {
+        return new ArrayList<>(inventory);
+    }
 }
